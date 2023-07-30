f37142b5a1851e1848e56d10ead1e1c24da2419e
Ardian Eka Candra <ardianoption@gmail.com>
2023-07-30 19:25:42 +0700

    test fetch to sst api

--- packages/expo-sst-auth/App.tsx
+++ packages/expo-sst-auth/App.tsx
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [tes, setTes] = useState<string>("Coba");
+
+  useEffect(() => {
+    setTimeout(
+      () =>
+        fetch("https://iqe6vss8sc.execute-api.ap-southeast-1.amazonaws.com")
+          .then((response) => response.text())
+          .then((data) => {
+            console.log("🚀 ~ file: App.tsx:12 ~ useEffect ~ data:", data);
+            setTes(data);
+          }),
+      1000
+    );
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{tes}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
