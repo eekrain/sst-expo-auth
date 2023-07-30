4a3ee6a815ff2b2dcb82ce28ca096e140dd5ea1b
Ardian Eka Candra <ardianoption@gmail.com>
2023-07-30 17:10:47 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A prisma test project";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = with pkgs; [
+            openssl
+            nodejs-slim
+            nodePackages.yarn
+          ];
+        };
+      });
+}
